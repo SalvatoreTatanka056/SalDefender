64c49698f01a69a9ea0347f8185805c5a3a8e38d
app-tate-a-tate <tatanka056@gmail.com>
2026-01-29 15:55:46 +0100

    ss

--- start_clamav.bat
+++ start_clamav.bat
@@ -1,4 +1,9 @@
 @echo off
-echo Starting ClamAV Daemon...
-"C:\Program Files\ClamAV\clamd.exe" -c "%~dp0clamd.conf" --foreground
-pause
+:: Avvia ClamAV in background
+start "" "C:\Program Files\ClamAV\clamd.exe" -c "%~dp0clamd.conf" --foreground
+
+:: Avvia SalDefender
+start "" "C:\Users\Avangarde\.gemini\antigravity\brain\32811d22-ea56-4491-97c9-c2b605c08512\SalDefender\bin\Debug\net8.0-windows\SalDefender.exe"
+
+:: Chiude immediatamente la finestra della shell
+exit
